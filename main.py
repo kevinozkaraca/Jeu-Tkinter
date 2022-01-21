d30efd58ce7b501f3b20bd5e1acd324457f21587
kevinozkaraca <kevin.ozkaraca@gmail.com>
2022-01-21 17:32:57 +0100

    Mise en place des fonctions

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import tkinter
 
+
 # Taille des éléments
 width_fenetre = "1055"
 height_fenetre = "750"
+
 
 # Affichage et parametre de la fenetre
 fenetre = tkinter.Tk()
@@ -10,6 +12,7 @@ fenetre.title("Jeu de la vie")
 fenetre.geometry(f"{width_fenetre}x{height_fenetre}")
 fenetre.resizable(width=False, height=False)
 fenetre.configure(background="#A1D2E1")
+
 
 # Lettres et numéro des boutons
 lettres_de_lalphabet = ["A", "B", "C", "D", "E", "F", "G",
@@ -24,6 +27,7 @@ numéro_possible = ["01", "02", "03", "04", "05", "06",
                    "24", "25"
                    ]
 
+
 # Parametre des boutons et de la boucle d'affichage
 font_des_boutons = "Arial"
 taille_de_font_boutons = 7
@@ -32,20 +36,31 @@ largeur_des_boutons = hauteur_des_boutons*2
 espace_multiplie_entre_boutons = 30
 compteur_de_lignes = 0
 compteur_de_lettre = 0
-index_du_numero = 0
+index_du_numero = 1
+btn = tkinter.Button( )
+
+
+# Fonction print - test
+def helloWorld():
+    print("Hello je suis le bouton  "+ btn['text'])
+    
 
 # Boucle d'affichage des boutons
 while compteur_de_lettre < 25:
     for i in range(26):
-        btn = tkinter.Button(fenetre,
-                         text=f'{lettres_de_lalphabet[compteur_de_lettre]}{numéro_possible[i]}',
-                         font=(font_des_boutons, taille_de_font_boutons),
-                         height=hauteur_des_boutons,
-                         width=largeur_des_boutons,
+        btn = tkinter.Button(   fenetre,
+                                text=f'{lettres_de_lalphabet[compteur_de_lettre]}{numéro_possible[i]}',
+                                textvariable=f'{lettres_de_lalphabet[compteur_de_lettre]}{numéro_possible[i]}',
+                                font=(font_des_boutons, taille_de_font_boutons),
+                                height=hauteur_des_boutons,
+                                width=largeur_des_boutons,
+                                command=helloWorld
                          )
         btn.place(x=i*espace_multiplie_entre_boutons, y=compteur_de_lignes*espace_multiplie_entre_boutons)
+        index_du_numero = index_du_numero + 1
     compteur_de_lignes = compteur_de_lignes + 1
     compteur_de_lettre = compteur_de_lettre + 1
+
 
 # Mainloop
 fenetre.mainloop()
