96345de9f870be8ee045126e0d9217f98fb73f9c
kevinozkaraca <kevin.ozkaraca@gmail.com>
2022-01-21 22:27:54 +0100

    Pour l'instant ça print seulement

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from functools import partial
 import tkinter
 
 
@@ -15,52 +16,107 @@ fenetre.configure(background="#A1D2E1")
 
 
 # Lettres et numéro des boutons
-lettres_de_lalphabet = ["A", "B", "C", "D", "E", "F", "G",
-                        "H", "I", "J", "K", "L", "M", "N",
-                        "O", "P", "Q", "R", "S", "T", "U",
-                        "V", "W", "X", "Y"
-                        ]
-numéro_possible = ["01", "02", "03", "04", "05", "06",
-                   "05", "06", "07", "08", "09", "10",
-                   "11", "12", "14", "15", "16", "17",
-                   "18", "19", "20", "21", "22", "23",
-                   "24", "25"
-                   ]
-
-
-# Parametre des boutons et de la boucle d'affichage
-font_des_boutons = "Arial"
-taille_de_font_boutons = 7
-hauteur_des_boutons = 2
-largeur_des_boutons = hauteur_des_boutons*2
-espace_multiplie_entre_boutons = 30
-compteur_de_lignes = 0
-compteur_de_lettre = 0
-index_du_numero = 1
-btn = tkinter.Button( )
+lettres_de_lalphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+]
+numéro_possible = [
+    "01",
+    "02",
+    "03",
+    "04",
+    "05",
+    "06",
+    "05",
+    "06",
+    "07",
+    "08",
+    "09",
+    "10",
+    "11",
+    "12",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "19",
+    "20",
+    "21",
+    "22",
+    "23",
+    "24",
+    "25",
+]
 
 
 # Fonction print - test
-def helloWorld():
-    print("Hello je suis le bouton  "+ btn['text'])
-    
-
-# Boucle d'affichage des boutons
-while compteur_de_lettre < 25:
-    for i in range(26):
-        btn = tkinter.Button(   fenetre,
-                                text=f'{lettres_de_lalphabet[compteur_de_lettre]}{numéro_possible[i]}',
-                                textvariable=f'{lettres_de_lalphabet[compteur_de_lettre]}{numéro_possible[i]}',
-                                font=(font_des_boutons, taille_de_font_boutons),
-                                height=hauteur_des_boutons,
-                                width=largeur_des_boutons,
-                                command=helloWorld
-                         )
-        btn.place(x=i*espace_multiplie_entre_boutons, y=compteur_de_lignes*espace_multiplie_entre_boutons)
-        index_du_numero = index_du_numero + 1
-    compteur_de_lignes = compteur_de_lignes + 1
-    compteur_de_lettre = compteur_de_lettre + 1
+def helloWorld(text):
+    if text == "A01":
+        print("Hello je suis le bouton  ", text)
+    if text == "A02":
+        print("Hello je suis le bouton  ", text)
 
 
-# Mainloop
-fenetre.mainloop()
+def main():
+    # Boucle d'affichage des boutons
+    # Parametre des boutons et de la boucle d'affichage
+    font_des_boutons = "Arial"
+    taille_de_font_boutons = 7
+    hauteur_des_boutons = 2
+    largeur_des_boutons = hauteur_des_boutons * 2
+    espace_multiplie_entre_boutons = 30
+    compteur_de_lignes = 0
+    compteur_de_lettre = 0
+    index_du_numero = 1
+    btn = tkinter.Button()
+
+    while compteur_de_lettre < 25:
+        for i in range(26):
+            text = f"{lettres_de_lalphabet[compteur_de_lettre]}{numéro_possible[i]}"
+            btn = tkinter.Button(
+                fenetre,
+                text=text,
+                textvariable=f"{lettres_de_lalphabet[compteur_de_lettre]}{numéro_possible[i]}",
+                font=(font_des_boutons, taille_de_font_boutons),
+                height=hauteur_des_boutons,
+                width=largeur_des_boutons,
+                command=partial(helloWorld, text),
+            )
+            btn.place(
+                x=i * espace_multiplie_entre_boutons,
+                y=compteur_de_lignes * espace_multiplie_entre_boutons,
+            )
+            index_du_numero = index_du_numero + 1
+        compteur_de_lignes = compteur_de_lignes + 1
+        compteur_de_lettre = compteur_de_lettre + 1
+
+    # Mainloop
+    fenetre.mainloop()
+
+
+if __name__ == "__main__":
+    main()
