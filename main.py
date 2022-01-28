40967711217b51977c1b750332ea6d31062d77be
kevinozkaraca <kevin.ozkaraca@gmail.com>
2022-01-28 20:18:44 +0100

    Correction PEP8

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from faulthandler import disable
 from functools import partial
 from fonctions_damier import fonctions_damier
 from fonctions_menu import fonctions_menu
@@ -11,10 +10,9 @@ from tkinter import DISABLED, Menu
 fenetre = tkinter.Tk()
 fenetre.title("Jeu tkinter")
 fenetre.configure(background="#A5F2A5")
-#mettre en plein écran : (a décommenté)
-#fenetre.attributes('-fullscreen', True)
+# Mettre en plein écran : (a décommenté en cas de besoin)
+# Fenetre.attributes('-fullscreen', True)
 fenetre.iconbitmap('images/icon.ico')
-
 
 # Lettres et numéro des boutons (Index)
 lettres_de_lalphabet = [
@@ -29,6 +27,7 @@ numero_possible = [
     "21", "22", "23", "24", "25",
 ]
 places_des_boutons = []
+
 
 def damier_de_boutons():
     # Parametre des boutons et de la boucle d'affichage
@@ -45,8 +44,8 @@ def damier_de_boutons():
     while compteur_de_lettre < 25:
         for i in range(26):
             text = f"{lettres_de_lalphabet[compteur_de_lettre]}{numero_possible[i]}"
-            bg="gray",
-            fg="black",
+            bg = "gray",
+            fg = "black",
             btn = tkinter.Button(
                 fenetre,
                 text=text,
@@ -60,27 +59,26 @@ def damier_de_boutons():
             )
                    
             # Contour du jeu en noir
-            if 'A' in text or '25' in text or 'Y' in text or '01' in text :
+            if 'A' in text or '25' in text or 'Y' in text or '01' in text:
                 btn = tkinter.Button(
                             bg="black",
                             fg="black",
                             font=(font_des_boutons, taille_de_font_boutons),
                             height=hauteur_des_boutons,
                             width=largeur_des_boutons,
-                            state = DISABLED
+                            state=DISABLED
                             )
-            
-            
             btn.place(
-                x=10+i * espace_multiplie_entre_boutons,
-                y=10+ compteur_de_lignes * espace_multiplie_entre_boutons,
+                x=10 + i * espace_multiplie_entre_boutons,
+                y=10 + compteur_de_lignes * espace_multiplie_entre_boutons,
             )
-            places_des_boutons.append(f"{lettres_de_lalphabet[compteur_de_lettre]}{numero_possible[i]}"+" se trouve à x : "f"{10+i * espace_multiplie_entre_boutons}"+"  y : "f"{10+ compteur_de_lignes * espace_multiplie_entre_boutons}")
+            places_des_boutons.append(f"{lettres_de_lalphabet[compteur_de_lettre]}{numero_possible[i]}"+
+                                      " se trouve à x : "f"{10+i * espace_multiplie_entre_boutons}"+
+                                      "  y : "f"{10+ compteur_de_lignes * espace_multiplie_entre_boutons}")
             index_du_numero = index_du_numero + 1
         compteur_de_lignes = compteur_de_lignes + 1
         compteur_de_lettre = compteur_de_lettre + 1
         
-
 
 # Création du menu
 barre_de_menu = Menu(fenetre)
@@ -97,8 +95,8 @@ fenetre.config(menu=barre_de_menu)
 
 
 # Bouton demarrer le jeu
-bouton_demarrer = tkinter.Button(fenetre, text ="Cliquez ici pour demarrer la partie",command=fonctions_demarrer)
-bouton_demarrer.place(x=850,y=10)
+bouton_demarrer = tkinter.Button(fenetre, text="Cliquez ici pour demarrer la partie", command=fonctions_demarrer)
+bouton_demarrer.place(x=850, y=10)
 
 # Infos sur la place des boutons 
 
